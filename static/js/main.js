b24f6b73225e1122c5d98cd2136129ff7c547eb0
Vicente Rivera <vicente.drd@gmail.com>
2017-08-30 18:37:09 -0300

    Cambios requeridos por el cliente
    Se agregaron servicios a la seccion correspondiente
    y se cambio el sistema de subtitulos.
    
    Tambien se limpio parte del codigo, para satisfacer estandares
    de estilizado.

--- static/js/main.js
+++ static/js/main.js
@@ -1,80 +1,58 @@
-/***************************************************************************************************************
-||||||||||||||||||||||||||||         CUSTOM SCRIPT FOR LOGIS CARGO          ||||||||||||||||||||||||||||||||||||
-****************************************************************************************************************
-||||||||||||||||||||||||||||              TABLE OF CONTENT                  ||||||||||||||||||||||||||||||||||||
-****************************************************************************************************************
-****************************************************************************************************************
-1. revolution slider
-2. accrodion
-3. gallery fancybox activator 
-4. select menu
-5. client carousel
-6. counter number changer
-7. google map
-8. contact form validation
-9. sticky header
-10. gallery
-11. typed plugin
-12. Mobile Navigation
-13. testimonails carousel
-****************************************************************************************************************
-||||||||||||||||||||||||||||            End TABLE OF CONTENT                ||||||||||||||||||||||||||||||||||||
-****************************************************************************************************************/
-
 
 "use strict"; // Start of use strict
 
 // 1. revolution slider
 function revolutionSliderActiver () {
-	if ($('.rev_slider_wrapper #slider1').length) {
-		jQuery("#slider1").revolution({
-			sliderType:"standard",
-			sliderLayout:"auto",
-			delay:5000,
-			navigation: {
-				arrows:{enable:true} 
-			}, 
-			gridwidth:1170,
-			gridheight:860 
-		});
-	};
-	if ($('.rev_slider_wrapper #slider2').length) {
-		jQuery("#slider2").revolution({
-			sliderType:"standard",
-			sliderLayout:"auto",
-			delay:5000,
-			navigation: {
-				arrows:{enable:true} 
-			}, 
-			gridwidth:1170,
-			gridheight:690 
-		});
-	};
+  if ($('.rev_slider_wrapper #slider1').length) {
+    jQuery("#slider1").revolution({
+      sliderType:"standard",
+      sliderLayout:"auto",
+      delay:5000,
+      navigation: {
+        arrows:{enable:true} 
+      }, 
+      gridwidth:1170,
+      gridheight:860 
+    });
+  }
+  if ($('.rev_slider_wrapper #slider2').length) {
+    jQuery("#slider2").revolution({
+      sliderType:"standard",
+      sliderLayout:"auto",
+      delay:5000,
+      navigation: {
+        arrows:{enable:true} 
+      }, 
+      gridwidth:1170,
+      gridheight:690 
+      });
+  }
 }
+
 // 2. accrodion
 function accrodion () {
-	if ($('.accrodion-grp').length) {
+  if ($('.accrodion-grp').length) {
 		
-		$('.accrodion-grp').each(function () {
-			var accrodionName = $(this).data('grp-name');
-			var Self = $(this);
-			Self.addClass(accrodionName);
-			Self.find('.accrodion .accrodion-content').hide();
-			Self.find('.accrodion.active').find('.accrodion-content').show();
-			Self.find('.accrodion').each(function() {
-				$(this).find('.accrodion-title').on('click', function () {
-					if ($(this).parent().hasClass('active') === false ) {					
-						$('.accrodion-grp.'+accrodionName).find('.accrodion').removeClass('active');
-						$('.accrodion-grp.'+accrodionName).find('.accrodion').find('.accrodion-content').slideUp();
-						$(this).parent().addClass('active');					
-						$(this).parent().find('.accrodion-content').slideDown();	
-					};
-				});
-			});
-		});
-		
-	};
+    $('.accrodion-grp').each(function () {
+      var accrodionName = $(this).data('grp-name');
+      var Self = $(this);
+      Self.addClass(accrodionName);
+      Self.find('.accrodion .accrodion-content').hide();
+      Self.find('.accrodion.active').find('.accrodion-content').show();
+      Self.find('.accrodion').each(function() {
+        $(this).find('.accrodion-title').on('click', function() {
+          if ($(this).parent().hasClass('active') === false ) {					
+            $('.accrodion-grp.'+accrodionName).find('.accrodion').removeClass('active');
+            $('.accrodion-grp.'+accrodionName).find('.accrodion').find('.accrodion-content').slideUp();
+            $(this).parent().addClass('active');					
+            $(this).parent().find('.accrodion-content').slideDown();	
+          }
+        });
+      });
+    });		
+  }
 }
+
 // 3. gallery fancybox activator 
 function GalleryFancyboxActivator () {
   var galleryFcb = $('.fancybox');
@@ -88,144 +66,157 @@ function GalleryFancyboxActivator () {
     });
   }
 }
+
 // 4. select menu
 function selectMenu () {
-	if ($('.select-menu').length) {
-		$('.select-menu').selectmenu();
-	};
+  if ($('.select-menu').length) {
+    $('.select-menu').selectmenu();
+  };
 }
+
 // 5. client carousel
 function clientCarousel () {
-	if ($('.client-carousel').length) {
-		$('.client-carousel .owl-carousel').owlCarousel({
-		    loop: true,
-		    margin: 27,
-		    nav: false,
-		    dots: false,
-		    autoWidth: true,
-		    autoplay:true,
-		    autoplayTimeout:3000,
-		    autoplayHoverPause:true,
-		    responsive: {
-		        0:{
-		            items:1,
-		            autoWidth: false
-		        },
-		        480:{
-		            items:2,
-		            autoWidth: false
-		        },
-		        600:{
-		            items:3,
-		            autoWidth: false
-		        },
-		        1000:{
-		            items:6,
-		            autoWidth: false
-		        }
-		    }
-		});
-	};
+  if ($('.client-carousel').length) {
+    $('.client-carousel .owl-carousel').owlCarousel({
+      loop: true,
+      margin: 27,
+      nav: false,
+      dots: false,
+      autoWidth: true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true,
+      responsive: {
+        0:{
+          items:1,
+          autoWidth: false
+        },
+        480:{
+          items:2,
+          autoWidth: false
+        },
+        600:{
+          items:3,
+          autoWidth: false
+        },
+        1000:{
+          items:6,
+          autoWidth: false
+        }
+      }
+    });
+  }
 }
+
 // 6. counter number changer
 function CounterNumberChanger () {
-	var timer = $('.timer');
-	if(timer.length) {
-		timer.appear(function () {
-			timer.countTo();
-		})
-	}
+  var timer = $('.timer');
+  if(timer.length) {
+    timer.appear(function () {
+  	  timer.countTo();
+    });
+  }
 }
+
 // 7. google map
 function gMap () {
-	if ($('.google-map').length) {
-        $('.google-map').each(function () {
-        	// getting options from html 
-        	var mapName = $(this).attr('id');
-        	var mapLat = $(this).data('map-lat');
-        	var mapLng = $(this).data('map-lng');
-        	var iconPath = $(this).data('icon-path');
-        	var mapZoom = $(this).data('map-zoom');
-        	var mapTitle = $(this).data('map-title');
+  if ($('.google-map').length) {
+    $('.google-map').each(function () {
+      // getting options from html 
+      var mapName = $(this).attr('id');
+      var mapLat = $(this).data('map-lat');
+      var mapLng = $(this).data('map-lng');
+      var iconPath = $(this).data('icon-path');
+      var mapZoom = $(this).data('map-zoom');
+      var mapTitle = $(this).data('map-title');
 
-        	// if zoom not defined the zoom value will be 15;
-        	if (!mapZoom) {
-        		var mapZoom = 15;
-        	};
-        	// init map
-        	var map;
-            map = new GMaps({
-                div: '#'+mapName,
-                scrollwheel: false,
-                lat: mapLat,
-                lng: mapLng,
-                zoom: mapZoom
-            });
-            // if icon path setted then show marker
-            if(iconPath) {
-        		map.addMarker({
-	            	icon: iconPath,
-	                lat: mapLat,
-	                lng: mapLng,
-	                title: mapTitle
-	            });
-        	}
-        });  
-	};
+      // if zoom is not defined, the zoom value will be 15;
+      if (!mapZoom) {
+        var mapZoom = 15;
+      }
+      
+      // init map
+      var map = new GMaps({
+        div: '#'+mapName,
+        scrollwheel: false,
+        lat: mapLat,
+        lng: mapLng,
+        zoom: mapZoom
+      });
+      // if icon path is set then show marker
+      if(iconPath) {
+        map.addMarker({
+          icon: iconPath,
+          lat: mapLat,
+          lng: mapLng,
+          title: mapTitle
+        });
+      }
+    });  
+  }
 }
+
 // 8. contact form validation
 function contactFormValidation () {
-
-	if($('.contact-form').length){
-		$('.contact-form').validate({ // initialize the plugin
-			rules: {
-				name: {
-					required: true
-				},
-				email: {
-					required: true,
-					email: true
-				},
-				message: {
-					required: true
-				},
-				subject: {
-					required: true
-				}
-			},
-			submitHandler: function (form) { 
-				// sending value with ajax request
-				$.post($(form).attr('action'), $(form).serialize(), function (response) {
-					$(form).parent('div').append(response);
-					$(form).find('input[type="text"]').val('');
-					$(form).find('input[type="email"]').val('');
-					$(form).find('textarea').val('');
-				});
-				return false;
-			}
-		});
-	}
+  if($('.contact-form').length){
+    $('.contact-form').validate({ // initialize the plugin
+      rules: {
+        name: {
+          required: true
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        message: {
+          required: true
+        },
+        subject: {
+          required: true
+        }
+      },
+      submitHandler: function (form) { 
+        // sending value with ajax request
+        $.post($(form).attr('action'), $(form).serialize(), function (response) {
+          $(form).parent('div').append(response);
+          $(form).find('input[type="text"]').val('');
+          $(form).find('input[type="email"]').val('');
+          $(form).find('textarea').val('');
+        });
+        
+        return false;
+      }
+    });
+  }
 }
+
 // 9. sticky header
 function stickyHeader () {
-	if ($('.stricky').length) {
-		var strickyScrollPos = 100;
-		if($(window).scrollTop() > strickyScrollPos) {
-			$('.stricky').removeClass('fadeIn animated');
-	      	$('.stricky').addClass('stricky-fixed fadeInDown animated');
-		}
-		else if($(this).scrollTop() <= strickyScrollPos) {
-			$('.stricky').removeClass('stricky-fixed fadeInDown animated');
-	      	$('.stricky').addClass('slideIn animated');
-		}
-	};
+  if ($('.stricky').length) {
+    var strickyScrollPos = 100;
+
+    if ($(window).scrollTop() > strickyScrollPos) {
+      $('.stricky').removeClass('fadeIn animated');
+      $('.stricky').addClass('stricky-fixed fadeInDown animated');
+    } 
+    else if ($(this).scrollTop() <= strickyScrollPos) {
+      $('.stricky').removeClass('stricky-fixed fadeInDown animated');
+      $('.stricky').addClass('slideIn animated');
+    }
+  }
 }
+
 // 10. gallery
 function fleetGallery () {
-	if ($('.fleet-gallery').length) {
-		$('.fleet-gallery').mixItUp();
-	};
+  if ($('.fleet-gallery').length) {
+    $('.fleet-gallery').mixItUp();
+  };
 }
+
+/*
+ * Removed per client request
+ */
+/*
 // 11. typed plugin
 function typed () {
 	if ($(".typed").length) {
@@ -241,81 +232,86 @@ function typed () {
 	        resetCallback: function () { newTyped(); }
 	    });
     };
-}
+}*/
+
 // 12. Mobile Navigation
 function mobileNavToggler () {
-	if ($('.nav-holder').length) {
-		$('.nav-holder .nav-header button').on('click', function () {
-			$('.nav-holder .nav-footer').slideToggle();
-			return false;
-		});
-		$('.nav-holder li.has-submenu').children('a').append(function () {
-			return '<button class="dropdown-expander"><i class="fa fa-bars"></i></button>';    			
-		});
-		$('.nav-holder .nav-footer .dropdown-expander').on('click', function () {
-			$(this).parent().parent().children('.submenu').slideToggle();
-			console.log($(this).parents('li'));
-			return false;
-		});
+  if ($('.nav-holder').length) {
+    $('.nav-holder .nav-header button').on('click', function () {
+      $('.nav-holder .nav-footer').slideToggle();
+      return false;
+    });
 
-	}
+    $('.nav-holder li.has-submenu').children('a').append(function () {
+	  return '<button class="dropdown-expander"><i class="fa fa-bars"></i></button>';    			
+    });
+		
+    $('.nav-holder .nav-footer .dropdown-expander').on('click', function () {
+      $(this).parent().parent().children('.submenu').slideToggle();
+      console.log($(this).parents('li'));
+      return false;
+    });
+  }
 }
 
 // 13. testimonails carousel
 function testimonialsCarosuleGardener () {
-	if ($('.testimonial-box.with-carousel').length) {
-		$('.testimonial-box.with-carousel .owl-carousel').owlCarousel({
-		    loop: true,
-		    margin: 0,
-		    nav: false,
-		    dots: true,
-		    autoplay: true,
-		    autoplayHoverPause: true,
-		    responsive: {
-		        0:{
-		            items:1
-		        },
-		        480:{
-		            items:1
-		        },
-		        600:{
-		            items:1
-		        },
-		        1000:{
-		            items:1
-		        }
-		    }
-		});
-	}
+  if ($('.testimonial-box.with-carousel').length) {
+    $('.testimonial-box.with-carousel .owl-carousel').owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: false,
+      dots: true,
+      autoplay: true,
+      autoplayHoverPause: true,
+      responsive: {
+        0:{
+          items:1
+        },
+        480:{
+          items:1
+        },
+        600:{
+          items:1
+        },
+        1000:{
+          items:1
+        }
+      }
+    });
+  }
 }
 
 
 
 // instance of fuction while Document ready event	
 jQuery(document).on('ready', function () {
-	(function ($) {
-		revolutionSliderActiver();
-		accrodion();
-		selectMenu();		
-		CounterNumberChanger();
-		contactFormValidation();
-		gMap();
-		fleetGallery();
-		GalleryFancyboxActivator();
-		typed();
-		mobileNavToggler();
-		testimonialsCarosuleGardener();
-	})(jQuery);
+  (function ($) {
+    revolutionSliderActiver();
+    accrodion();
+    selectMenu();
+    CounterNumberChanger();
+    contactFormValidation();
+    gMap();
+    fleetGallery();
+    GalleryFancyboxActivator();
+    // Removed per client request
+    //typed();
+    mobileNavToggler();
+    testimonialsCarosuleGardener();
+  })(jQuery);
 });
 
 // instance of fuction while Window Load event
 jQuery(window).on('load', function () {
-	(function ($) {
-		clientCarousel();
-	})(jQuery);
+  (function ($) {
+    clientCarousel();
+  })(jQuery);
 });
 
-/* Se quita a solicitud del cliente
+/*
+ * Removed per client request
+ */
 /*
 // instance of fuction while Window Scroll event
 jQuery(window).on('scroll', function () {	
